4fb55c7cb920e968710fdf4cb3120cb94550334a
WangPing521 <wp_scdedu@163.com>
2021-12-11 14:45:34 +0800

    lastcheckpromise12

--- Promise12_plugin0.08.sh
+++ Promise12_plugin0.08.sh
@@ -8,7 +8,10 @@ source $CC_WRAPPER_PATH
 
 time=8
 account=def-chdesa
-save_dir=promise_cavat_0.08_2runs
+save_dir=promise_lastcheck
+
+ratio1=0.05
+unlab_ratio1=$(python -c "print(1-${ratio1})")
 
 ratio2=0.08
 unlab_ratio2=$(python -c "print(1-${ratio2})")
@@ -20,34 +23,48 @@ declare -a StringArray=(
 # local connectivity, size of convolution = 3, 7, 13
 
 #cavat ru=0
-#"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous ConstraintWeightScheduler.max_value=0.001   Trainer.save_dir=${save_dir}/promise_201cons Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-#"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous ConstraintWeightScheduler.max_value=0.001   Trainer.save_dir=${save_dir}/promise_201cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-#"python -O main.py seed=12345 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous ConstraintWeightScheduler.max_value=0.001   Trainer.save_dir=${save_dir}/promise_201cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous ConstraintWeightScheduler.max_value=0.002 Trainer.save_dir=${save_dir}/promise_202cons_run1 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous  ConstraintWeightScheduler.max_value=0.002 Trainer.save_dir=${save_dir}/promise_202cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=12345 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous   ConstraintWeightScheduler.max_value=0.002 Trainer.save_dir=${save_dir}/promise_202cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+
+"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous ConstraintWeightScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/promise_205cons_run1 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous  ConstraintWeightScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/promise_205cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=12345 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous   ConstraintWeightScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/promise_205cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+
+"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous ConstraintWeightScheduler.max_value=0.0006 Trainer.save_dir=${save_dir}/promise_306cons_run1 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous  ConstraintWeightScheduler.max_value=0.0006 Trainer.save_dir=${save_dir}/promise_306cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=12345 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous   ConstraintWeightScheduler.max_value=0.0006 Trainer.save_dir=${save_dir}/promise_306cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+
+"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous ConstraintWeightScheduler.max_value=0.0002 Trainer.save_dir=${save_dir}/promise_302cons_run1 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous  ConstraintWeightScheduler.max_value=0.0002 Trainer.save_dir=${save_dir}/promise_302cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=12345 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous   ConstraintWeightScheduler.max_value=0.0002 Trainer.save_dir=${save_dir}/promise_302cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+
+"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous ConstraintWeightScheduler.max_value=0.00006 Trainer.save_dir=${save_dir}/promise_406cons_run1 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous  ConstraintWeightScheduler.max_value=0.00006 Trainer.save_dir=${save_dir}/promise_406cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=12345 Arch.num_classes=2 Dataset=prostate Trainer.name=constraintReg Constraints.Constraint=connectivity Constraints.Connectivity.credit_type=discretecontinuous   ConstraintWeightScheduler.max_value=0.00006 Trainer.save_dir=${save_dir}/promise_406cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+
 
 
 # Cavat
 # reg=0.0001, 0.0005, 0.005, 0.001
 # eps=0.1, 0.001, 0.0001, 0.00001
 # cons=0.0005, 0.0001, 0.00005, 0.00001
-"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.001   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps201cons Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=123456  Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.001   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps201cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=12345   Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.001   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps201cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-
 #"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.0005   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps305cons Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.0005   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps305cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=12345  Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.0005   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps305cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+#"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.0005   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps305cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+#"python -O main.py seed=12345  Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.0005   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps305cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
 
-#"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.0001   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps301cons Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.0001   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps301cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=12345  Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.0001   RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps301cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+# MT
+"python -O main.py seed=1234567 Arch.num_classes=2 Optim.lr=0.00001 Dataset=prostate Trainer.name=MeanTeacher RegScheduler.max_value=0.5 Trainer.save_dir=${save_dir}/MT_prostate_05reg_run1 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=123456 Arch.num_classes=2 Optim.lr=0.00001 Dataset=prostate Trainer.name=MeanTeacher  RegScheduler.max_value=0.5 Trainer.save_dir=${save_dir}/MT_prostate_05reg_run2 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=12345 Arch.num_classes=2 Optim.lr=0.00001 Dataset=prostate Trainer.name=MeanTeacher   RegScheduler.max_value=0.5 Trainer.save_dir=${save_dir}/MT_prostate_05reg_run3 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
 
-#"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.00005  RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps405cons Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.00005  RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps405cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=12345  Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.00005  RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps405cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=1234567 Arch.num_classes=2 Optim.lr=0.00001 Dataset=prostate Trainer.name=MeanTeacher RegScheduler.max_value=1 Trainer.save_dir=${save_dir}/MT_prostate_1reg_run1 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=123456 Arch.num_classes=2 Optim.lr=0.00001 Dataset=prostate Trainer.name=MeanTeacher  RegScheduler.max_value=1 Trainer.save_dir=${save_dir}/MT_prostate_1reg_run2 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=12345 Arch.num_classes=2 Optim.lr=0.00001 Dataset=prostate Trainer.name=MeanTeacher   RegScheduler.max_value=1 Trainer.save_dir=${save_dir}/MT_prostate_1reg_run3 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
 
-#"python -O main.py seed=1234567 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.00001  RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps401cons Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=123456 Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.00001  RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps401cons_run2 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
-"python -O main.py seed=12345  Arch.num_classes=2 Dataset=prostate Trainer.name=consVat VATsettings.pertur_eps=1 Constraints.Constraint=connectivity ConstraintWeightScheduler.max_value=0.00001  RegScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cavatcons_205reg1eps401cons_run3 Data.unlabeled_data_ratio=${unlab_ratio2} Data.labeled_data_ratio=${ratio2}"
+"python -O main.py seed=1234567 Arch.num_classes=2 Optim.lr=0.00001 Dataset=prostate Trainer.name=MeanTeacher RegScheduler.max_value=2 Trainer.save_dir=${save_dir}/MT_prostate_2reg_run1 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=123456 Arch.num_classes=2 Optim.lr=0.00001 Dataset=prostate Trainer.name=MeanTeacher  RegScheduler.max_value=2 Trainer.save_dir=${save_dir}/MT_prostate_2reg_run2 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=12345 Arch.num_classes=2 Optim.lr=0.00001 Dataset=prostate Trainer.name=MeanTeacher   RegScheduler.max_value=2 Trainer.save_dir=${save_dir}/MT_prostate_2reg_run3 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
 
 
 #mtCAVAT
