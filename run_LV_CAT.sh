ccce9b3240730d0b7b019415401ac5f8d57772c1
WangPing521 <wp_scdedu@163.com>
2022-06-15 00:11:47 -0400

    CAT_lv

--- run_LV_CAT.sh
+++ run_LV_CAT.sh
@@ -8,15 +8,25 @@ source $CC_WRAPPER_PATH
 
 time=6
 account=def-chdesa
-save_dir=run_LV
+save_dir=0614_LV_conv_CAT
 
 ratio1=0.03
 unlab_ratio1=$(python -c "print(1-${ratio1})")
 
-ratio2=0.05
-unlab_ratio2=$(python -c "print(1-${ratio2})")
-
 declare -a StringArray=(
+# co-training_0.03
+"python -O main.py seed=1234567 Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=co_training RegScheduler.max_value=0.001 Trainer.save_dir=${save_dir}/cot_205reg_run1 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=123456  Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=co_training RegScheduler.max_value=0.001 Trainer.save_dir=${save_dir}/cot_205reg_run2 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=12345   Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=co_training RegScheduler.max_value=0.001 Trainer.save_dir=${save_dir}/cot_205reg_run3 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+
+"python -O main.py seed=1234567 Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=co_training RegScheduler.max_value=0.001 Trainer.save_dir=${save_dir}/cot_201reg_run1 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=123456  Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=co_training RegScheduler.max_value=0.001 Trainer.save_dir=${save_dir}/cot_201reg_run2 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=12345   Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=co_training RegScheduler.max_value=0.001 Trainer.save_dir=${save_dir}/cot_201reg_run3 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+
+"python -O main.py seed=1234567 Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=co_training RegScheduler.max_value=0.0005 Trainer.save_dir=${save_dir}/cot_305reg_run1 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=123456  Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=co_training RegScheduler.max_value=0.0005 Trainer.save_dir=${save_dir}/cot_305reg_run2 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+"python -O main.py seed=12345   Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=co_training RegScheduler.max_value=0.0005 Trainer.save_dir=${save_dir}/cot_305reg_run3 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
+
 
 # CaVAT_0.03(no adv)
 "python -O main.py seed=1234567 Arch.num_classes=2 Optim.lr=0.00001 Dataset=acdc Foreground=LV Trainer.name=constraintReg Constraints.Constraint=convexity ConstraintWeightScheduler.max_value=0.005 Trainer.save_dir=${save_dir}/cons_205cons_run1 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
