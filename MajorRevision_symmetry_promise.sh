484e09ee6307e38a523db703ed74f361060c6e72
WangPing521 <wp_scdedu@163.com>
2022-10-25 15:13:13 -0400

    simplify

--- MajorRevision_symmetry_promise.sh
+++ MajorRevision_symmetry_promise.sh
@@ -13,8 +13,6 @@ save_dir=1021_symmetry_promise
 ratio1=0.08
 unlab_ratio1=$(python -c "print(1-${ratio1})")
 
-ratio2=0.05
-unlab_ratio2=$(python -c "print(1-${ratio2})")
 
 declare -a StringArray=(
 "python -O main.py seed=1234567 Data.seed=1 Arch.num_classes=2 Dataset=prostate Foreground=all Trainer.name=consVat Constraints.Constraint=symmetry VATsettings.pertur_eps=1 Constraints.reward_type=hard Constraints.examples=adv_examples ConstraintWeightScheduler.max_value=0.005  RegScheduler.max_value=0.0000001 Trainer.checkpoint_path=runs/${save_dir}/CAT_symmetry_1r_601reg_205cons_run1 Trainer.save_dir=${save_dir}/CAT_symmetry_1r_601reg_205cons_run1 Data.unlabeled_data_ratio=${unlab_ratio1} Data.labeled_data_ratio=${ratio1}"
